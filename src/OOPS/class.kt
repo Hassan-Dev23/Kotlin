e5cd683ff0388a91453cf1620f02af93ae8aa821
Muhammad Hassan <m.ha77an99@gmail.com>
2024-10-25 22:50:22 +0500

    Added some topics

--- src/OOPS/class.kt
+++ src/OOPS/class.kt
@@ -1,4 +1,22 @@
-class DailyTask{
+
+class DailyTask(hourparm:Int,name:String){
+     lateinit var stepsgoal : String
+
+
+//     Getter and setters
+     val name = name
+    get(){
+        return field.toUpperCase()
+    }
+
+     var hours = hourparm
+        set(value){
+          if (value in 1..24) {
+              field=value
+          }
+            else println("Please go and do some work")
+          }
+
     fun wake(){
         println("wake up early in the morning")
     }
@@ -8,14 +26,19 @@ class DailyTask{
     fun DSALecture(){
         println("Continue your DSA lecture")
     }
+    fun walk(){
+        println("Today $name walked $stepsgoal steps which is my goal")
+    }
 
 
 }
 
 
 fun main(){
-val hassan=DailyTask()
+val hassan=DailyTask(0,"Hassan")
+    hassan.stepsgoal=" 50 "
     hassan.wake()
     hassan.Ibadda()
     hassan.DSALecture()
+    hassan.walk()
 }
